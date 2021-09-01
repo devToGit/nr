62a73ebd63d2bce8ce94706e0c8a0d5b7f9ccef7
devToGit <ret@DESKTOP-8R3GMS2.localdomain>
2021-09-01 18:36:00 +0300

    empty-SSR-config

--- src/server/indexTemplate.js
+++ src/server/indexTemplate.js
@@ -1,12 +1,15 @@
+export const indexTemplate = () => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reddit</title>
-<script defer src="index.js"></script></head>
+    <title>Reddit 2</title>
+    <script src="/static/client.js" type="application/javascript"></script>
+</head>
 <body>
     <div id="react_root"></div>
 </body>
 </html>
+`;
